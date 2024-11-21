c1b1521bab791dbe63d9e6378358ab0e174aac19
Elnumer01 <eromario.marve@gmail.com>
2024-11-20 21:04:18 -0600

    cambio vistas

--- resources/views/components/Books/ModalUpdate.blade.php
+++ resources/views/components/Books/ModalUpdate.blade.php
@@ -24,10 +24,11 @@
                 @enderror
                 <input value="{{$row->gender}}" class="form-control" name="gender" placeholder="Genero" aria-label="Username"><br>
                 @error('gender')
-                    <small class="text-danger">{{$message}}</small>
+                    <small class="text-danger">{{$message}}</small><br>
                 @enderror
                 <label for="">Autores</label>
                 <select class="form-select" name="author_id" id="">
+                    <option value="">Selecciona un autor...</option>
                     @foreach ($authors as $author)
                         <option value="{{$author->id}}">{{$author->name}} {{$author->lastname}}</option>
                     @endforeach
